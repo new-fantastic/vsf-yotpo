89f17a564bfcfa4615df20b3e02f856bb923704f
Fifciuu <filip.jdrasik@gmail.com>
2019-10-09 12:08:59 +0200

    Fetching totals should be in layout for a proper storeCode

--- hooks/afterRegistration.ts
+++
@@ -1,5 +0,0 @@
-export async function afterRegistration({ Vue, config, store, isServer }) {
-  if (isServer && !store.state['vsf-yotpo'].totals) {
-    await store.dispatch('vsf-yotpo/loadTotals');
-  }
-}
--- index.ts
+++ index.ts
@@ -1,17 +1,10 @@
 import { module } from './store';
-// import { plugin } from './store/plugin'
-import { afterRegistration } from './hooks/afterRegistration';
 import { createModule } from '@vue-storefront/core/lib/module';
-// import { beforeEach } from './router/beforeEach'
-// import { afterEach } from './router/afterEach'
-// import { initCacheStorage } from "@vue-storefront/core/helpers/initCacheStorage";
 import { KEY } from './const';
 
-// export const cacheStorage = initCacheStorage(KEY);
 export const VsfYotpo = createModule({
   key: KEY,
-  store: { modules: [{ key: KEY, module }] },
-  afterRegistration
+  store: { modules: [{ key: KEY, module }] }
 });
 
 /* Tests to write
